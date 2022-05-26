9880e1e8540864a705c46034c86905746f3f0901
Ronan Cunningham <ronan.cunningham@harness.io>
2022-05-26 11:50:36 +0100

    update to terraform

--- terraform-provision-gke-cluster/gke.tf
+++ terraform-provision-gke-cluster/gke.tf
@@ -26,7 +26,22 @@ resource "google_container_cluster" "primary" {
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
+
+  cluster_autoscaling {
+    enabled = true
+    resource_limits {
+      resource_type = "cpu"
+      minimum = 1
+      maximum = 40
+    }
+    resource_limits {
+      resource_type = "memory"
+      minimum = 4
+      maximum =120
+    }
+  }
 }
+
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
